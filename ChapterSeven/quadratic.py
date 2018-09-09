450a0013d6ac3037288a8300e4aa96f0029de65e
Wayne Gross <waynegross1@gmail.com>
2018-09-09 14:13:30 -0400

    Wrapped up Chapter 7

---
+++ ChapterSeven/quadratic.py
@@ -0,0 +1,18 @@
+# quadratic.py
+# A program that computes the real roots of a quadratic equation.
+# Note: this program crashes is the equation has no real roots.
+
+import math
+
+def main():
+    print("This program finds the real solutions to a quadratic\n")
+
+    a, b, c = eval(input("Please enter the coefficients (a, b, c): "))
+
+    discRoot = math.sqrt(b * b - 4 * a * c)
+    root1 = (-b + discRoot) / (2 * a)
+    root2 = (-b - discRoot)/ (2 * a)
+
+    print("\nThe solutions are:", root1, root2)
+
+main()
